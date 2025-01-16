7bfd29574d82d9eeb542a8bcd61be1c080961a32
jcvillacob <jucaviza6@gmail.com>
2025-01-16 14:17:38 -0500

    fix: font size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { InfoComponent } from "./componentes/info/info.component";
 import * as AOS from 'aos';
 import { FooterComponent } from "./componentes/footer/footer.component";
@@ -8,16 +7,16 @@ import { HeroComponent } from "./componentes/hero/hero.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InfoComponent, FooterComponent, HeroComponent],
+  imports: [InfoComponent, FooterComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   ngOnInit() {
     AOS.init({
-      duration: 1000,        // Duración de la animación
-      once: true,            // Si quieres que ocurra solo una vez
-      offset: 300,           // Offset global en px
+      duration: 1000,
+      once: true,
+      offset: 300,
       easing: 'ease-out-cubic'
     });
   }
